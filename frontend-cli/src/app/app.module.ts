68cc5ec6e360f79155545b4c3c8d1cd19a23174b
Jochen Kapaun <jokap@yahoo.com>
2016-10-18 17:47:42 +0200

    remove bar component

--- frontend-cli/src/app/app.module.ts
+++ frontend-cli/src/app/app.module.ts
@@ -8,15 +8,13 @@ import { AppComponent } from './app.component';
 import { OverviewComponent } from './overview/overview/overview.component';
 import { MainComponent } from './vote/main/main.component';
 import { routing }  from './app-routing.module';
-import { BarComponent } from './vote/bar/bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     OverviewComponent,
-    MainComponent,
-    BarComponent
+    MainComponent
   ],
   imports: [
     BrowserModule,
