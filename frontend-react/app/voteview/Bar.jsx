2b30f1d504461eccfcfe4309278b3b448fcffd1d
Jochen Kapaun <jokap@yahoo.com>
2016-10-25 10:26:37 +0200

    button order

--- frontend-react/app/voteview/Bar.jsx
+++ frontend-react/app/voteview/Bar.jsx
@@ -13,13 +13,13 @@ const Bar = React.createClass({
       const percentageDelta = absoluteDelta / 1000;
       return (
           <div key={this.props.basevalueId}>
-             <p>{this.props.category} - {this.props.basevalue} Mio. Euro</p>             
+             <p>{this.props.category} - {this.props.basevalue} Mio. Euro</p>
              <LinearProgress mode="determinate" value={percentageBase} />
              <p>Mein Vorschlag: {absoluteDelta} Mio. Euro</p>
              <LinearProgress mode="determinate" value={percentageDelta} />
+             <FlatButton label="minus 5 milliarden" primary={true} onClick={this.props.onSetDelta.bind(null, this.props.basevalueId, (this.props.delta || 0) - 5000)} />
              <FlatButton label="plus 5 milliarden" primary={true} onClick={this.props.onSetDelta.bind(null, this.props.basevalueId, (this.props.delta || 0) + 5000)} />
-             <FlatButton label="minus 5 milliarden" primary={true} onClick={this.props.onSetDelta.bind(null, this.props.basevalueId, (this.props.delta || 0) - 5000)} />             
-          </div> 
+          </div>
       );
     }
 });
